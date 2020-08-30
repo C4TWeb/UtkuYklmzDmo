8fb2da5069d0511a8fd90b44efeebee09c30ac91
C4TWeb <43504055+C4TWeb@users.noreply.github.com>
2020-08-30 16:33:20 +0400

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,49 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const ayarlar = require('./ayarlar.json');
+const chalk = require('chalk');
+const moment = require('moment');
+var Jimp = require('jimp');
+const { Client, Util } = require('discord.js');
+const weather = require('weather-js')
+const fs = require('fs');
+const db = require('quick.db');
+const http = require('http');
+const express = require('express');
+require('./util/eventLoader')(client);
+const path = require('path');
+const request = require('request');
+const snekfetch = require('snekfetch');
+const queue = new Map();
+const YouTube = require('simple-youtube-api');
+const ytdl = require('ytdl-core');
+
+
+const app = express();
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Ping Tamamdır.");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+
+var prefix = ayarlar.prefix;
+
+const log = message => {
+    console.log(`${message}`);
+};
+
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+fs.readdir('./komutlar/', (err, files) => {
+    if (err) console.error(err);
+    log(`${files.length} komut yüklenecek.`);
+    files.forEach(f => {
+        let props = require(`./komutlar/${f}`);
+        log(`Yüklenen komut: ${props.help.name}.`);
+        client.commands.set(props.help.name, props);
+        props.conf.aliases.forEach(alias => {
+            client.aliases.set(alias, props.help.name);
+        });
